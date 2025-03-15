60ef65a628ebf88f2df7b40adca97617d7cc701f
liuzhi1247 <2524587016@qq.com>
2025-03-15 15:22:31 +0800

    Create LC1049.cpp

---
+++ LC1049.cpp
@@ -0,0 +1,21 @@
+# https://leetcode.cn/problems/last-stone-weight-ii/
+
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        // 1 <= stones.length <= 30, 1 <= stones[i] <= 100
+        // sum should be the half
+        vector<int> dp(1501, 0);
+        int sum = accumulate(stones.begin(), stones.end(), 0);
+        int total = sum / 2;
+
+        for(int i = 0; i<stones.size(); ++i) {
+            for(int j = total; j >= stones[i]; --j ) {
+                dp[j] = max(dp[j], dp[j - stones[i]] + stones[i]);
+            }
+        }
+        //one sumup is dp[total], the other sumup is sum - dp[total]
+        // sum - dp[total] is bigger than dp[total].
+        return sum - dp[total] * 2;
+    }
+};
